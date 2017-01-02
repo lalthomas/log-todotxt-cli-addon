efdd16113db959ee0d327a451b3c0a9ada269854
Lal Thomas <lal.thomas.mail@gmail.com>
2017-01-02 22:27:56 +0530

    add run file

--- log run.bat
+++ log run.bat
@@ -4,3 +4,11 @@ REM Creation Date : 2017-01-02
 REM Author : Lal Thomas 	
 REM Original File : D:\Dropbox\project\20160204-log todotxt cli addon\log 	
 	
+%~d0
+cd "%~p0"
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
+pushd "%userProfile%\.todo.actions.d"
+echo "%scriptFolderPath%"
+mklink /d log "%scriptFolderPath%"
+popd
