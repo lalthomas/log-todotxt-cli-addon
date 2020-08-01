d28928450be9b9107808897802c5701687e7fd6c
Îalthomas <lal.thomas.mail@gmail.com>
2020-08-01 16:53:01 +0700

    Update log run.bat

--- log run.bat
+++ log run.bat
@@ -1,8 +1,7 @@
 @echo OFF 	
 REM File : log run.bat 	
 REM Creation Date : 2017-01-02 	
-REM Author : Lal Thomas 	
-REM Original File : D:\Dropbox\project\20160204-log todotxt cli addon\log 	
+REM Author : Lal Thomas	
 	
 %~d0
 cd "%~p0"
